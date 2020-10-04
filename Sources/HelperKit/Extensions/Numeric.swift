6a7613552204a2dff930ae4ec6cb446712093c98
Mario Nachbaur <marionauta@users.noreply.github.com>
2020-10-04 12:38:26 +0200

    feat(Numeric.units): add doc comments

--- Sources/HelperKit/Extensions/Numeric.swift
+++ Sources/HelperKit/Extensions/Numeric.swift
@@ -1,6 +1,12 @@
 import Foundation
 
 public extension Numeric {
+    /// Returns the amount of screen points in given units.
+    ///
+    /// A unit represents 8 screen points.
+    ///
+    ///     let padding = 2.units // 16 points
+    ///     let margin = 2.5.units // 20 points
     var units: Self {
         return self * 8
     }
