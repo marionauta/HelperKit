b7d0919be8072ede6a07c1de5302400905592937
Mario Nachbaur <marionauta@users.noreply.github.com>
2020-10-04 13:57:55 +0200

    feat(UIEdgeInsets): add parameter descriptions to doc comments

--- Sources/HelperKit/Extensions/UIEdgeInsets.swift
+++ Sources/HelperKit/Extensions/UIEdgeInsets.swift
@@ -5,6 +5,8 @@ public extension UIEdgeInsets {
     /// Creates an inset with the same amount on each side.
     ///
     ///     let inset = UIEdgeInsets(all: 16)
+    ///
+    /// - Parameter all: inset for every side.
     init(all insets: CGFloat) {
         self.init(top: insets, left: insets, bottom: insets, right: insets)
     }
@@ -14,6 +16,10 @@ public extension UIEdgeInsets {
     ///     let inset1 = UIEdgeInsets(horizontal: 16, vertical: 8)
     ///     let inset2 = UIEdgeInsets(horizontal: 24)
     ///     let inset3 = UIEdgeInsets(vertical: 32)
+    ///
+    /// - Parameters:
+    ///     - horizontal: inset for left & right sides.
+    ///     - vertical: inset for top & bottom sides.
     init(horizontal hInsets: CGFloat = .zero, vertical vInsets: CGFloat = .zero) {
         self.init(top: vInsets, left: hInsets, bottom: vInsets, right: hInsets)
     }
