c8b2ddc5f13c4b4d5dae68a516391dbf4b2cba7f
Mario Nachbaur <marionauta@users.noreply.github.com>
2020-10-04 13:07:04 +0200

    feat(Array): safe indexing through subscript

---
+++ Sources/HelperKit/Extensions/Array.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+public extension Array {
+    /// Accesses the element at the specified position, if in bounds.
+    ///
+    ///     let array = [10, 20, 30, 40]
+    ///     array[safe: 3] // 40
+    ///     array[safe: 4] // nil
+    ///
+    @inlinable subscript(safe index: Int) -> Element? {
+        guard index >= startIndex, index < endIndex else { return nil }
+        return self[index]
+    }
+}
