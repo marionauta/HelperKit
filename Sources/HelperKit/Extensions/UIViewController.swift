6b2466a90fe102e8999dc5ee13b4d76dae5b2699
Mario Nachbaur <marionauta@users.noreply.github.com>
2020-10-24 12:19:50 +0200

    feat(UIViewController.safeArea): add doc comments

--- Sources/HelperKit/Extensions/UIViewController.swift
+++ Sources/HelperKit/Extensions/UIViewController.swift
@@ -2,6 +2,8 @@
 import UIKit
 
 public extension UIViewController {
+    /// Shorthand for the view controller's view safe area layout guide.
+    /// The portion of the view that is unobscured by bars and other content.
     @available(iOS 11.0, *)
     var safeArea: UILayoutGuide {
         view.safeAreaLayoutGuide
