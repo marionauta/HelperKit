0943507d980fb6bfec1237d155cace0484b29774
Javier García López <javigarcialopez@gmail.com>
2020-12-15 11:38:53 +0100

    feat(UIView): add function to allow adding several subviews at once

---
+++ Sources/HelperKit/Extensions/UIView.swift
@@ -0,0 +1,19 @@
+import Foundation
+import UIKit
+
+extension UIView {
+    /// Allows to add multiple subviews to the current view, the amount can be variable
+    ///
+    ///     let view1 = UIView()
+    ///     let view2 = UIView()
+    ///     ...
+    ///     let viewN = UIView()
+    ///     view.addSubviews(view1, view2, ..., viewN)
+    ///
+    /// - Parameter views: The views to add as subviews
+    public func addSubviews(_ views: UIView...) {
+        for view in views {
+            self.addSubview(view)
+        }
+    }
+}
