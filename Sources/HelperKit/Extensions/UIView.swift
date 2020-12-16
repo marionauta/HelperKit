e3893e5ebd4e167f0be4678788e9d48954b3bfea
Mario Nachbaur <marionauta@users.noreply.github.com>
2020-12-16 17:50:59 +0100

    feat(UIView.addSubviews): update doc comment

--- Sources/HelperKit/Extensions/UIView.swift
+++ Sources/HelperKit/Extensions/UIView.swift
@@ -2,15 +2,25 @@
 import UIKit
 
 extension UIView {
-    /// Allows to add multiple subviews to the current view, the amount can be variable
+    /// Adds multiple subviews to the current view. The amount can be variable.
     ///
     ///     let view1 = UIView()
     ///     let view2 = UIView()
-    ///     ...
+    ///     // ...
     ///     let viewN = UIView()
-    ///     view.addSubviews(view1, view2, ..., viewN)
+    ///     view.addSubviews(view1, view2, viewN)
     ///
-    /// - Parameter views: The views to add as subviews
+    /// Calls to this method can be nested, providing a clear representation of the view tree.
+    ///
+    ///     let view1 = UIView()
+    ///     let view2 = UIView()
+    ///     let view3 = UIView()
+    ///     view.addSubviews(
+    ///         view1,
+    ///         view2.addSubviews(view3)
+    ///     )
+    ///
+    /// - Parameter views: The views to add as subviews.
     @discardableResult
     public func addSubviews(_ views: UIView...) -> UIView {
         for view in views {
