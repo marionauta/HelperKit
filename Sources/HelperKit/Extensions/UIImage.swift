a06ca7eb2e4d31028f4359cea5a73a26c0a8e1bc
Mario Nachbaur <marionauta@users.noreply.github.com>
2021-02-20 12:47:33 +0100

    feat(UIImage): create from color

---
+++ Sources/HelperKit/Extensions/UIImage.swift
@@ -0,0 +1,22 @@
+#if canImport(UIKit)
+import UIKit
+
+extension UIImage {
+    /// Creates an `UIImage` from the specified `color` and `size`.
+    ///
+    ///     let imageOne = UIImage.from(color: .red)
+    ///
+    ///     let size = CGSize(width: 10, height: 10)
+    ///     let imageTen = UIImage.from(color: .red, size: size)
+    ///
+    /// - Parameters:
+    ///     - color: The color to fill the image with.
+    ///     - size: The size of the resulting image. Defaults to 1x1.
+    public static func from(color: UIColor, size: CGSize = .one) -> UIImage {
+        return UIGraphicsImageRenderer(size: size).image { context in
+            color.setFill()
+            context.fill(.init(origin: .zero, size: size))
+        }
+    }
+}
+#endif
