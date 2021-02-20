f4ce95809c1d0504a695b8fe8034672d0a1d47ec
Mario Nachbaur <marionauta@users.noreply.github.com>
2021-02-20 12:40:14 +0100

    feat(CGSize): add `one` size

---
+++ Sources/HelperKit/Extensions/CGSize.swift
@@ -0,0 +1,9 @@
+#if canImport(CoreGraphics)
+import CoreGraphics
+
+extension CGSize {
+    /// The size whose width and height are both one.
+    public static let one = CGSize(width: 1, height: 1)
+}
+
+#endif
