0779c1c901b63794e89a83aacd8d86fe8dbf4014
Mario Nachbaur <marionauta@users.noreply.github.com>
2021-05-23 13:17:04 +0200

    feat(Sequence): move `Optional` methods from `Array`

--- Sources/HelperKit/Extensions/Sequence+Optional.swift
+++ Sources/HelperKit/Extensions/Sequence+Optional.swift
@@ -1,15 +1,15 @@
 import Foundation
 
-extension Array {
+extension Sequence {
     /// Unwraps and filters out `nil` elements.
-    public func filterNil<T>() -> Array<T> where Element == Optional<T> {
+    public func filterNil<T>() -> [T] where Element == Optional<T> {
         return filter(\.isSome).map(\.unsafelyUnwrapped)
     }
 
     /// Unwraps and replaces `nil` elements with `default`.
     ///
     /// - Parameter default: The value to use when `nil` is encountered.
-    public func replaceNilWith<T>(default: T) -> Array<T> where Element == Optional<T> {
+    public func replaceNilWith<T>(default: T) -> [T] where Element == Optional<T> {
         return map { $0 ?? `default` }
     }
 }
