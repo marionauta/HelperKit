c352f1f3ea01a9008caa54c3849b16ad858a991c
Mario Nachbaur <marionauta@users.noreply.github.com>
2021-10-20 18:51:48 +0200

    feat(Array): add deprecation warning to `chunked(into:)`

--- Sources/HelperKit/Extensions/Array.swift
+++ Sources/HelperKit/Extensions/Array.swift
@@ -21,6 +21,7 @@ extension Array {
     ///     array.chunked(into: 3) // [[1, 2, 3], [4, 5, 6], [7, 8]]
     ///
     /// - Parameter into: chunk size.
+    @available(*, deprecated, message: "Use `chunks(ofCount:)` from swift-algorithms")
     public func chunked(into size: Int) -> [[Element]] {
         return stride(from: 0, to: count, by: size).map {
             Array(self[$0 ..< Swift.min($0 + size, count)])
