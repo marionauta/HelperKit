e8f924e39e481c7e3394b99822bddd13ab1765bf
Mario Nachbaur <marionauta@users.noreply.github.com>
2023-11-25 12:30:56 +0100

    feat(String): add "empty" string helpers

--- Sources/HelperKit/Extensions/String.swift
+++ Sources/HelperKit/Extensions/String.swift
@@ -15,3 +15,50 @@ extension String {
         String(format: localized, arguments: arguments)
     }
 }
+
+// MARK: - Empty
+
+extension String {
+    /// The empty string
+    public static let empty = ""
+
+    /// Returns `nil` if the string is empty
+    @inlinable
+    public var nilIfEmpty: String? {
+        isEmpty ? nil : self
+    }
+
+    /// Returns `self` if it has characters, or `replacement` if it doesn't.
+    ///
+    /// - Parameter replacement: the value that replaces `self` when it's empty.
+    @inlinable
+    public func whenEmpty(_ replacement: String) -> String {
+        isEmpty ? replacement : self
+    }
+}
+
+extension Optional where Wrapped == String {
+    /// A Boolean value indicating whether a string has no characters.
+    @inlinable
+    public var isEmpty: Bool {
+        switch self {
+        case .none:
+            return true
+        case let .some(wrapped):
+            return wrapped.isEmpty
+        }
+    }
+
+    /// Returns `self` if it has characters, or `replacement` if it doesn't.
+    ///
+    /// - Parameter replacement: the value that replaces `self` when it's empty.
+    @inlinable
+    public func whenEmpty(_ replacement: String) -> String {
+        switch self {
+        case .none:
+            return replacement
+        case let .some(wrapped):
+            return wrapped.whenEmpty(replacement)
+        }
+    }
+}
